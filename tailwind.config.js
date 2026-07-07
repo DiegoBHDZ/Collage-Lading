c6aef7228bd18fba4a48aea04fa2ddfb8c805534
DiegoBHDZ <diego.barreraher@gmail.com>
2026-07-07 15:48:09 -0600

    fix: correccion de tamaños v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",          // <-- ¡Añadimos el radar para la raíz!
-    "./src/**/*.{html,js}"   // Mantenemos esto para que siga detectando tus animaciones.js
+    "./index.html",          
+    "./src/**/*.{html,js}"  
   ], 
   theme: {
     extend: {
       colors: {
-        'brand-red': '#f5683a',
+        'brand-red': '#f83e02',
         'brand-light': '#fde7cf',
         'brand-dark': '#000000'
       },
       fontFamily: {
-        // Títulos oficiales
-        'title': ['"ITC Benguiat Condensed Bold"', 'serif'],
-        // Texto principal oficial
-        'body': ['"ITC Franklin Gothic LT"', 'sans-serif'],
+        // Títulos
+        'title': ['"Sunborn"', 'sans-serif'],
+        // Texto principal
+        'body': ['"ITC Franklin Gothic LT"',],
       }
     },
   },
